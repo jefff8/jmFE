f35d675513bf0b411e736869992300ef1583c3d5
jeff8 <360531371@qq.com>
2019-01-06 16:13:23 +0800

    2019年1月6日16:13:13

--- js/service.js
+++ js/service.js
@@ -1,8 +1,9 @@
 /*
  *服务器参数
  */
-url='http://192.168.0.112:8082/CI/jmadmin/';//宿舍 
+//url='http://192.168.0.112:8082/CI/jmadmin/';//602 
+//url='http://112.74.34.150:888/CI/jmadmin/';//同欣服务器
 //url='http://202.105.179.167:8080/jmConstr/jmadmin/';//服务器
-//url='http://182.61.39.167/jmConstr/jmadmin/';//服务器
+url='http://182.61.39.167/jmConstr/jmadmin/';//服务器
 //var servername='root';
 //var serverpassword='123456';
